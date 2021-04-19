ce02be0e09f61f9c239146e354e86c35fbb0bd61
Your Name <you@example.com>
2021-04-19 06:59:10 +0300

    create and style landing page

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    $("#desImg").click(function(){
+        $("#desImg").slideDown('1500').hide('1000');
+        $("#desText").slideDown('1500').hide('1000');
+        $("#design").show('1500');
+      });
+      $("#design").click(function(){
+        $("#design").slideUp('1500');
+        $("#desImg").slideDown('1500');
+        $("#desText").slideDown('1500');
+      });
+    $("#devImg").click(function(){
+      $("#devImg").slideDown('1500').hide('1000');
+      $("#devText").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#devImg").slideDown('1500');
+      $("#devText").slideDown('1500');
+    });
+      $("#prodImg").click(function(){
+          $("#prodImg").slideDown('1500').hide('1000');
+          $("#prodText").slideDown('1500').hide('1000');
+          $("#prod-management").show('1500');
+      });
+      $("#prod-management").click(function(){
+          $("#prod-management").slideUp('1500');
+          $("#prodImg").slideDown('1500');
+          $("#prodText").slideDown('1500');
+      });
+     $('.portImg').hover(function(){
+          $('.porttext',this).slideToggle('slow');
+     }, function(){
+         $('.porttext',this).slideToggle('slow');
+      });
+
+      $("form#form").on('submit',function(event){
+        event.preventDefault();
+        var name = $("input#name").val();
+        var email = $("input#email").val();
+        var message = $("textarea#message").val();
+
+        if ($("input#name").val() && $("input#email").val()){
+            alert ("Hey " + name + ", Thanks for reaching out to us.");
+        }
+        else {
+            alert("Please provide your correct name and email!");
+        }
+
+
+  });
